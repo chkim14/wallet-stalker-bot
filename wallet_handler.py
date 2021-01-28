395768d9d1bd6a5ee37de9e3efac20bd62153e5c
lilpieceofshit <lilpieceofshit69@yahoo.com>
2021-01-28 08:20:00 +0000

    added cases for handling a new coin/liquidating a coin,
    added usd value in message to discord,
    
    added "SIG_CHANGE" const that determines USD value a transaction needs to exceed to trigger a message,
    
    changed value type for dictionaries wallet_diffs, holdings_dict (added coin's usd value),
    
    added debugging print statements to see if those areas are being reached

--- wallet_handler.py
+++ wallet_handler.py
@@ -4,6 +4,9 @@ import requests
 from replit import db
 from bs4 import BeautifulSoup
 
+# minimum amount (in usd) to trigger update
+SIG_CHANGE = 5
+
 def get_wallet_holdings(wallet_num):
   headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Firefox/50.0'}
   url = os.getenv('PREFIX_URL') + wallet_num + os.getenv('SUFFIX_URL')
@@ -11,13 +14,16 @@ def get_wallet_holdings(wallet_num):
   raw_html = requests.get(url, headers=headers).text
   parsed_html = BeautifulSoup(raw_html,'html.parser')
 
+  # holdings_dict[coin_name]: [amount, usd_value]
   holdings_dict = {}
   for trgt_html in parsed_html.findAll('td',class_='clsShowAlert'):
     token = trgt_html.findAll('td', class_=None)
     if len(token) > 1:
       coin_name = token[1].text
       units = token[2].text.replace(',','')
-      holdings_dict.update({coin_name:units})
+      # added new dicttionary value to track usd value of coin
+      usd_val = token[3].text[1:].split(' ')[0]
+      holdings_dict.update({coin_name:[units, usd_val]})
 
   return holdings_dict
 
@@ -29,16 +35,41 @@ def wallet_exists(wallet_num):
 
 def update_db(wallet_num,holdings_dict): 
   db[wallet_num] = holdings_dict
+  print("update")
 
 def detect_wallet_changes(wallet_num,curr_holdings):
+  # wallet_diffs[key]: (diff_in_units, diff_in_usd)
   wallet_diffs = {}
   stored_holdings = db[wallet_num]
+  # stored_holdings[key]: [amount, usd_value]
   for key in stored_holdings: 
     if key in curr_holdings:
-      if float(curr_holdings[key]) != float(stored_holdings[key]): 
-        holding_diff = float(curr_holdings[key]) - float(stored_holdings[key])
+      if float(curr_holdings[key][0]) != float(stored_holdings[key][0]):
+        # store both diff in units and diff and usd
+        diff_units = float(curr_holdings[key][0]) - float(stored_holdings[key][0])
+        diff_usd = diff_units * float(curr_holdings[key][1])
+        holding_diff = [diff_units, diff_usd] #float(curr_holdings[key]) - float(stored_holdings[key])
+        wallet_diffs.update({key:holding_diff})
+      # wallet owner liquidated all of this coin
+      elif key not in curr_holdings:
+        print("liquidate coin")
+        print(-float(stored_holdings[key][0]))
+        # store both diff in units and diff and usd
+        diff_units = -float(stored_holdings[key][0])
+        diff_usd = -float(stored_holdings[key][0]) * float(stored_holdings[key][1])
+        holding_diff = [diff_units, diff_usd]
         wallet_diffs.update({key:holding_diff})
 
+  # check if wallet owner added new coin
+  for key in curr_holdings:
+    # wallet owner added new coin
+    if key not in stored_holdings:
+        print("new coin")
+        # store both diff in units and diff and usd
+        diff_units = float(curr_holdings[key][0])
+        diff_usd = float(curr_holdings[key][0]) * float(curr_holdings[key][1])
+        holding_diff = [diff_units, diff_usd]
+        wallet_diffs.update({key:holding_diff})
   return wallet_diffs 
 
 async def scan_wallets(client):
@@ -46,6 +77,7 @@ async def scan_wallets(client):
   channel = client.get_channel(int(os.getenv('CHANNEL')))
 
   while True:
+    print("loop")
     with open('wallets.txt') as f:
       ids = f.read().splitlines()
     for id in ids:
@@ -53,15 +85,27 @@ async def scan_wallets(client):
 
       if wallet_exists(id): 
         wallet_diffs = detect_wallet_changes(id,holdings_dict)
-        for key in wallet_diffs: 
-          amnt_diff = "{:.5f}".format(abs(wallet_diffs[key]))
-          if wallet_diffs[key] > 0: 
-            out_msg = '{}    :arrow_up: {}#{}'.format(id,amnt_diff,key)
-          else: 
-            out_msg = '{}    :arrow_down: {}#{}'.format(id,amnt_diff,key)
-          await channel.send(out_msg)
+        for key in wallet_diffs:
+          print("loop")
+          # difference in native units 
+          diff_units = "{:.5f}".format(abs(wallet_diffs[key][0]))
+          # difference in usd value
+          diff_usd = "{:.5f}".format(abs(wallet_diffs[key][1]))
+          if wallet_diffs[key][1] > SIG_CHANGE: 
+            print("sending message")
+            out_msg = '{}    :arrow_up: {}#{}, {}#USD'.format(id,diff_units,key,diff_usd)
+            await channel.send(out_msg)
+          elif wallet_diffs[key][1] < -SIG_CHANGE: 
+            print("sending message")
+            out_msg = '{}    :arrow_down: {}#{}, {}#USD'.format(id,diff_units,key,diff_usd)
+            await channel.send(out_msg)
+          else:
+            # debug, remove line if works
+            print(id, ": ", key, ": ", wallet_diffs[key][1])
+          
           update_db(id,holdings_dict)
       else: 
+        print("else")
         update_db(id,holdings_dict)
 
     await asyncio.sleep(300)
